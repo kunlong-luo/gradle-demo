ed3685304d56d0ef79da4ca6d87fb112918ea11c
kunlong-luo <kunlong_luo@163.com>
2023-08-21 09:11:01 +0800

    feat: add some repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,33 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.1.2"
-	id("io.spring.dependency-management") version "1.1.2"
+    java
+    id("org.springframework.boot") version "3.1.2"
+    id("io.spring.dependency-management") version "1.1.2"
 }
 
 group = "com.loongstudio"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+	maven {
+		url = uri("https://maven.aliyun.com/repository/public/")
+	}
+	maven {
+		url = uri("https://maven.aliyun.com/repository/central")
+	}
+    mavenLocal()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
